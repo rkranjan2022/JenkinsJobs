c8d6a26d2a0181ac62d6485b935571c5f8050858
rkranjanjnu <ranjeet.ranjan08@gmail.com>
2022-09-11 11:31:34 +0530

    file added

---
+++ abc.java
@@ -0,0 +1,14 @@
+ Volume in drive C is Windows
+ Volume Serial Number is 22A2-C39D
+
+ Directory of C:\Users\rkran\Documents\Jenkins\JavaProject
+
+11-09-2022  11:30    <DIR>          .
+11-09-2022  10:39    <DIR>          ..
+11-09-2022  11:30                 0 abc.java
+11-09-2022  11:26               469 Hello.class
+04-09-2022  10:22               147 hello.java
+11-09-2022  11:25               462 java2.class
+11-09-2022  11:21               140 java2.java
+               5 File(s)          1,218 bytes
+               2 Dir(s)  17,969,516,544 bytes free
